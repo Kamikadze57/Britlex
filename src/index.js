12438602a02c247c20cc118615af91ecccd4bc61
Kamikadze57 <143871458+Kamikadze57@users.noreply.github.com>
2024-06-19 20:47:08 +0300

    fix menu #2

--- src/index.js
+++ src/index.js
@@ -29,13 +29,16 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-mobile-open]'),
-    closeModalBtn: document.querySelector('[data-mobile-close]'),
-    modal: document.querySelector('[data-mobile]'),
+    openModalBtn: document.querySelector("[data-mobile-open]"),
+    closeModalBtn: document.querySelector("[data-mobile-close]"),
+    modal: document.querySelector("[data-mobile]"),
   };
 
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
   }
 })();
